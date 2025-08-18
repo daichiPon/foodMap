aa9ef05aba5c6796b0691d3d714e4aae6c037528
daichiPon <nakamotodaich@icloud.com>
2025-08-19 00:02:30 +0900

    amplify.configuerの修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 // src/App.tsx
 import React from 'react';
 import { Amplify } from 'aws-amplify';
-import awsconfig from './aws-exports';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import MapWithPinForm from './mapCmp/mapPage';
