9314d9cfb48803d787559673745daa86288e7689
daichiPon <nakamotodaich@icloud.com>
2025-08-26 23:40:11 +0900

    pwa化＆iconの変更

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { registerSW } from 'virtual:pwa-register'
 
-console.log(1)
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("新しいバージョンがあります。更新しますか？")) {
+      updateSW(true) // 返り値を使って更新
+    }
+  },
+  onOfflineReady() {}
+})
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
